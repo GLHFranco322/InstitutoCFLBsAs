2797a818d97cd0e69cbc4e66acbab229d5613661
Franco Spiazzi <francospiazzi96@gmail.com>
2025-06-23 13:55:09 -0300

    section

--- src/components/AllCurses.jsx
+++ src/components/AllCurses.jsx
@@ -3,8 +3,10 @@ import './AllCurses.css';
 const AllCurses = () => {
   return (
     <article id='all-curses'>
-      <img src="./icons/curses.png" alt="" />
-      <h3>Nuestros cursos</h3>
+      <div className="container-all">
+        <img src="./icons/curses.png" alt="" />
+        <h3>Nuestros cursos</h3>
+      </div>
     </article>
   )
 }
--- src/components/sections/SectionTwo.jsx
+++ src/components/sections/SectionTwo.jsx
@@ -7,21 +7,21 @@ const SectionTwo = () => {
         <section id='section-two'>
             <article className="container-section-two">
                 <ul className="stickers-container">
-                    <Sticker number={1} tittle={'Auxiliar de Farmacia'} marginLeft={'10rem'}/>
-                    <Sticker number={3} tittle={'Auxiliar de Farmacia'} marginLeft={'0'}/>
-                    <Sticker number={2} tittle={'Auxiliar de Farmacia'} marginLeft={'-5rem'}/>
-                    <Sticker number={4} tittle={'Auxiliar de Farmacia'} marginLeft={'-5rem'}/>
-                    <Sticker number={5} tittle={'Auxiliar de Farmacia'} marginLeft={'0'}/>
-                    <Sticker number={6} tittle={'Auxiliar de Farmacia'} marginLeft={'10rem'}/>
+                    <Sticker number={1} tittle={'Auxiliar de Farmacia'} marginLeft={'5rem'}/>
+                    <Sticker number={3} tittle={'Extraccionista con manejo de laboratorio'} marginLeft={'-3rem'}/>
+                    <Sticker number={2} tittle={'Acompañante terapeutico'} marginLeft={'-7rem'}/>
+                    <Sticker number={4} tittle={'Integración escolar'} marginLeft={'-7rem'}/>
+                    <Sticker number={5} tittle={'Reparación y programación de celulares'} marginLeft={'-3rem'}/>
+                    <Sticker number={6} tittle={'Electricidad domiciliaria'} marginLeft={'5rem'}/>
                 </ul>
                 <AllCurses />
                 <ul className="stickers-container">
-                    <Sticker number={7} tittle={'Auxiliar de Farmacia'} marginRight={'10rem'}/>
-                    <Sticker number={8} tittle={'Auxiliar de Farmacia'} marginRight={'0'}/>
-                    <Sticker number={9} tittle={'Auxiliar de Farmacia'} marginRight={'-5rem'}/>
-                    <Sticker number={10} tittle={'Auxiliar de Farmacia'} marginRight={'-5rem'}/>
-                    <Sticker number={11} tittle={'Auxiliar de Farmacia'} marginRight={'0rem'}/>
-                    <Sticker number={12} tittle={'Auxiliar de Farmacia'} marginRight={'10rem'}/>
+                    <Sticker number={7} tittle={'Aire acondicionado y refrigeración'} marginRight={'5rem'}/>
+                    <Sticker number={8} tittle={'Barbería'} marginRight={'-3rem'}/>
+                    <Sticker number={9} tittle={'Tatuajes'} marginRight={'-7rem'}/>
+                    <Sticker number={10} tittle={'Manicuría'} marginRight={'-7rem'}/>
+                    <Sticker number={11} tittle={'Masoterapia'} marginRight={'-3rem'}/>
+                    <Sticker number={12} tittle={'Cosmetología, cejas y pestañas'} marginRight={'5rem'}/>
                 </ul>
             </article>
         </section>
